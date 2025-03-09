d9aab88f22f7362b0f24c2b5fda834f87b0b2a24
aravindnaidu09 <aravindbabu372@gmail.com>
2025-03-09 12:56:44 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
-import { Routes } from '@angular/router';
+import { provideRoutes, Routes, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { CareersComponent } from './pages/careers/careers.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [   
-        { path: '', component: HomeComponent },
-        // { path: 'about/:section', component: AboutComponent },
-        // { path: 'services/:type', component: ServicesComponent },
-        // { path: 'products/:productName', component: ProductsComponent },
-        { path: 'privacy-policy', component: PrivacyPolicyComponent },
-        { path: 'careers', component: CareersComponent },
-        { path: 'contact-us', component: ContactUsComponent },
-        { path: '**', redirectTo: '' } // Redirect unknown routes
-      ];
-    
-      
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'careers', component: CareersComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: '**', redirectTo: '' }
+];
+
