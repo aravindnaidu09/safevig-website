9782e263ffec7abe94802356d4225d00d0e74f6a
Sharath Bhavaraju <89680328+sharath2601@users.noreply.github.com>
2025-06-19 21:42:06 +0530

    Changes Made

--- src/app/pages/social-icons/social-icons.component.ts
+++ src/app/pages/social-icons/social-icons.component.ts
@@ -9,9 +9,9 @@ import { Component } from '@angular/core';
 })
 export class SocialIconsComponent {
   socialLinks = [
-    { name: 'Facebook', icon: './../../../assets/Icons/Social/facebook.png', link: 'https://www.facebook.com' },
-    { name: 'Twitter', icon: './../../../assets/Icons/Social/twitter.png', link: 'https://www.twitter.com' },
-    { name: 'LinkedIn', icon: './../../../assets/Icons/Social/linkedin.png', link: 'https://www.linkedin.com' },
+    // { name: 'Facebook', icon: './../../../assets/Icons/Social/facebook.png', link: 'https://www.facebook.com' },
+    // { name: 'Twitter', icon: './../../../assets/Icons/Social/twitter.png', link: 'https://www.twitter.com' },
+    { name: 'LinkedIn', icon: './../../../assets/Icons/Social/linkedin.png', link: 'https://www.linkedin.com/company/95700798/admin/dashboard/' },
     { name: 'YouTube', icon: './../../../assets/Icons/Social/youtube.png', link: 'https://www.youtube.com' }
   ];
 }
